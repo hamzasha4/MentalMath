e741e35314fb2cfe5f3812f2d9f051f5bee6c36c
hamzasha4 <67180225+hamzasha4@users.noreply.github.com>
2021-09-20 01:05:31 +0500

    new project

---
+++ Default.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ExampleApp._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+<div class="text-center">
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="question" runat="server" Font-Bold="True" Font-Names="Arial Black" Font-Size="60px" ForeColor="#0099FF" Text="Questions"></asp:Label>
+&nbsp;<br />
+    <br />
+    <br />
+    <br />
+    Enter your Answer Below<br />
+    <br />
+    <asp:TextBox ID="AnswerTextBox" runat="server" ></asp:TextBox>
+    <br />
+    <br />
+
+    <asp:Button ID="SubmitButton" runat="server" OnClick="Button1_Click" Text="Submit" CssClass="btn btn-success" />
+    <br />
+    <br />
+    <asp:Label ID="answer" runat="server"></asp:Label>
+    <br />
+   
+    <br />
+    
+</div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ExampleApp
+{
+    
+    public partial class _Default : Page
+    {
+        private int ans = 0;
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            
+                var r = new Random();
+                int a = r.Next(0, 9);
+                int b = r.Next(0, 9);
+                question.Text = a.ToString() + " + " + b.ToString();
+                ans = a + b;
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(AnswerTextBox.Text);
+            answer.Text = x.ToString();
+            if (x == ans)
+            {
+                answer.Text = "You got the Correct Answer";
+            }
+            else 
+            {
+                answer.Text = "The Answer is Wrong || Correct Answer is " + ans.ToString();
+            }
+        }
+ 
+    }
+}
